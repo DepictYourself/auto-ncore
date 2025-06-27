3f194d571263fb87c5b2ac765fd0d995ca7a22eb
DepictYourself <totha014@gmail.com>
2025-06-27 11:22:06 +0200

    Change backend log file path

--- backend/start.sh
+++ backend/start.sh
@@ -7,4 +7,4 @@ pkill -f "uvicorn main:app" 2>/dev/null
 export $(cat .env.production | xargs)
 
 # Start backend
-nohup /opt/bin/uvicorn main:app --host 0.0.0.0 --port 8000 > /tmp/uvicorn.log 2>&1 &
+nohup /opt/bin/uvicorn main:app --host 0.0.0.0 --port 8000 > /tmp/mnt/Intenso/auto-ncore/backend/uvicorn.log 2>&1 &
